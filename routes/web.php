3afca03fc61d6138dc9adf870c965fec8d6aa240
Sandeep Kushwaha <160579425+shrisandeepkushwaha@users.noreply.github.com>
2024-06-25 03:00:21 +0530

    Meet Eloquent

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/jobs', function () {
+    $jobs = App\Models\Job::all();
+    dd($jobs[1]->salary);
+});
