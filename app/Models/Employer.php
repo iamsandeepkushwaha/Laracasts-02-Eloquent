c1a1d1c288edd71cc537c45835c2f52595e0713d
Sandeep Kushwaha <160579425+shrisandeepkushwaha@users.noreply.github.com>
2024-06-25 17:21:23 +0530

    Two Key Eloquent Relationship Types

--- app/Models/Employer.php
+++ app/Models/Employer.php
@@ -4,9 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Employer extends Model
 {
     use HasFactory;
     protected $fillable = ['name'];
+
+    /**
+     * Get the Jobs for the Employer.
+     */
+    public function jobs(): HasMany
+    {
+        return $this->hasMany(Job::class);
+    }
 }
--- app/Models/Job.php
+++ app/Models/Job.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Job extends Model
 {
@@ -11,4 +12,10 @@ class Job extends Model
 
     protected $table = 'job_listings';
     protected $fillable = ['title', 'salary'];
+
+    public function employer(): BelongsTo
+    {
+        // code...
+        return $this->belongsTo(Employer::class);
+    }
 }
