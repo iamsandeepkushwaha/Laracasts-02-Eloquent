340de747da9576a5b83f29bb03ff153149f0c718
Sandeep Kushwaha <160579425+shrisandeepkushwaha@users.noreply.github.com>
2024-06-29 16:57:41 +0530

    Forms and CSRF

--- routes/web.php
+++ routes/web.php
@@ -4,23 +4,39 @@ use Illuminate\Support\Facades\Route;
 use App\Models\Job;
 
 Route::get('/', function () {
-    return view('home');
+    return to_route('/jobs');
 });
 
 Route::get('/jobs', function () {
     // $jobs = Job::with('employer')->Paginate(3);
     // $jobs = Job::with('employer')->simplePaginate(3);
-    $jobs = Job::with('employer')->cursorPaginate(3);
+    $jobs = Job::with('employer')->latest()->cursorPaginate(3);
 
-    return view('jobs', [
+    return view('jobs.index', [
         'jobs' => $jobs
     ]);
+});
+
+Route::get('/jobs/create', function () {
+    return view('jobs.create');
 });
 
 Route::get('/jobs/{id}', function ($id) {
     $job = Job::find($id);
 
-    return view('job', ['job' => $job]);
+    return view('jobs.show', ['job' => $job]);
+});
+
+Route::post('/jobs', function () {
+    // validation...
+
+    Job::create([
+        'title' => request('title'),
+        'salary' => request('salary'),
+        'employer_id' => 1
+    ]);
+
+    return redirect('/jobs');
 });
 
 Route::get('/contact', function () {
